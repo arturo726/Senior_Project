f10a1d7c1b1e349bab4daf789150a58dc6c76292
arturo726 <amartinez726@hotmail.com>
2016-05-05 19:48:07 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,15 +1,14 @@
 <?php
 
-
+// Name: Artruo Martinez
+// Due: May 4, 2016
+// Senior Project
+// Purpose: The file will connect to the database. 
+    
  
 // Create connection
 $con=mysql_connect("cs-database.cs.loyola.edu","gmejia","1643829","beachelectric") or die("could not connect");
  
-// Check connection
-
-
-
-
 
 
 
