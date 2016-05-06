e5d25392aa7f4a16dc05d6b110489e6fa2331ef0
arturo726 <amartinez726@hotmail.com>
2016-05-05 20:09:28 -0400

    Update sessionActive.php

--- sessionActive.php
+++ sessionActive.php
@@ -1,12 +1,17 @@
 <?php
 
+// Name: Artruo Martinez
+// Due: May 4, 2016
+// Senior Project
+// Purpose: The file check to see what user is logged in.
 
 session_start();
 $userCheck = $_SESSION['userLogin'];
 
-if(!isset($userCheck)) {
+if(!isset($userCheck)) 
+{
 
-		      mysql_close();
+	mysql_close();
 
 }
 
